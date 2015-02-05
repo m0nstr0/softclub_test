f80198cc2aeab3cb9b4ea433d2c7efa5a0d1e025
Sergey Bolotaev <sergey@bolotaev.com>
2015-02-05 22:34:07 +0400

    CRUD for emails have been added

--- www/protected/views/layouts/main.php
+++ www/protected/views/layouts/main.php
@@ -29,7 +29,8 @@
 	<div id="mainmenu">
 		<?php $this->widget('zii.widgets.CMenu',array(
 			'items'=>array(
-				array('label'=>'Home', 'url'=>array('/site/index')),
+				array('label'=>'Подписка на новости', 'url'=>array('/site/index')),
+				array('label'=>'Список е-mail', 'url'=>array('/emails/index'), 'visible'=>!Yii::app()->user->isGuest),
 				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
 				array('label'=>'Contact', 'url'=>array('/site/contact')),
 				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
